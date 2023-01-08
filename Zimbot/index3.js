88c80bad9b4dd5c7aabe39f5aff7f29d376db31a
nodamembandel <bukannodamembandel@gmail.com>
2023-01-08 07:28:10 +0700

    Update index3.js

--- Zimbot/index3.js
+++ Zimbot/index3.js
@@ -7,8 +7,8 @@
 //▮
 //▮REPORT ABUSE OF THIS SOFTWARE EMAIL US
 //▮reinhardtuna@mail.uk
-//▮WHATSAPP US : +44 7441 437150
-//▮YOUTUBE CHANNELL: https://youtube.com/c/DRIPSOFC
+//▮WHATSAPP US : +6281216328228
+//▮YOUTUBE CHANNELL:
 //▮
 //╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 //
